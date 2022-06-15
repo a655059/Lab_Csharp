c8720d641c6288b2102e7009e8ea02edd6b330ad
ShawnShen <a655059@hotmail.com.tw>
2022-06-15 16:47:03 +0800

    123

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            Console.Write("enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WindowsFormsApp1/frm_M02_Hello.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class frm_M02_Hello
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_M02_Hello));
+            this.btnHello = new System.Windows.Forms.Button();
+            this.labName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHello
+            // 
+            this.btnHello.BackColor = System.Drawing.SystemColors.Control;
+            this.btnHello.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnHello.Location = new System.Drawing.Point(328, 245);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(135, 73);
+            this.btnHello.TabIndex = 0;
+            this.btnHello.Text = "Hello";
+            this.btnHello.UseVisualStyleBackColor = false;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // labName
+            // 
+            this.labName.AutoSize = true;
+            this.labName.BackColor = System.Drawing.Color.Transparent;
+            this.labName.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labName.Location = new System.Drawing.Point(270, 215);
+            this.labName.Name = "labName";
+            this.labName.Size = new System.Drawing.Size(53, 16);
+            this.labName.TabIndex = 1;
+            this.labName.Text = "Name: ";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtName.Location = new System.Drawing.Point(328, 212);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(135, 27);
+            this.txtName.TabIndex = 2;
+            // 
+            // frm_M02_Hello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(485, 339);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.labName);
+            this.Controls.Add(this.btnHello);
+            this.Name = "frm_M02_Hello";
+            this.Text = "Hello Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Label labName;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
